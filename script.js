880f149986ae34925183b9e17354d2e3bc87c3a6
Rajeev Marada <88943874+RajeevMarada@users.noreply.github.com>
2025-07-19 12:02:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// We will add more JavaScript here to populate the timeline and project grid later.
+// For now, this provides the smooth scrolling functionality.
